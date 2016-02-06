f65af1bbdefad6fbda7ccd0f92678168c7e09ae5
jOscillada06 <joscillada06@gmail.com>
2016-02-06 13:11:07 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,11 +1,7 @@
-data<- read.table("household_power_consumption.txt", sep=";", header=T, quote= "", strip.white=TRUE, stringsAsFactors = F, na.strings= "?")
-
-# Subsetting the full data to obtain the data related to two days: 
-data<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
-
-# creating Plot1
-png("plot1.png", width=480, height= 480)
-
-hist(data$Global_active_power, col= "red", xlab= "Global Active Power (kilowatts)", ylab= "Frequency", main= "Global Active Power")
-
-dev.off()
+plot1 <- function() {
+        hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+        dev.copy(png, file="plot1.png", width=480, height=480)
+        dev.off()
+        cat("Plot1.png has been saved in", getwd())
+}
+plot1()
